0b0227926c829fd08a22547f2201825703cf96fe
Wing leung CHOI <wingleungchoi@Wings-MacBook-Pro.local>
2017-08-13 16:18:26 +0800

    feat/notification basic

---
+++ src/module.js
@@ -0,0 +1,68 @@
+// @flow
+
+const PREFIX = '@@ketchup/'
+
+const ADD = `${PREFIX}ADD_KETCHUP_NOTIFICATION`
+const REMOVE = `${PREFIX}REMOVE_KETCHUP_NOTIFICATION`
+
+export {
+  ADD as ADD_KETCHUP_NOTIFICATION,
+  REMOVE as REMOVE_KETCHUP_NOTIFICATION
+}
+
+function createNotification(id, text, type, timeout, dismissAfter) {
+  return {
+    id,
+    text,
+    type,
+    timeout,
+    dismissAfter
+  }
+}
+
+export function createModule(options: KetchupOptions = {}) {
+  const {
+    dismissInterval = 3000
+  } = options
+
+  let maxKetchupId = 0
+
+  const state: KetchupState = {
+    notifications: []
+  }
+
+  const getters = {
+    ketchupNotifications: (state: KetchupState) => state.notifications
+  }
+
+  const actions = {
+    [ADD] ({ commit }, { text, type = 'info', timeout = true, dismissAfter = dismissInterval }) {
+      const id = ++maxKetchupId
+      const notification = createNotification(id, text, type, timeout, dismissAfter)
+
+      commit(ADD, notification)
+      setTimeout(() => commit(REMOVE, id), dismissAfter)
+    },
+
+    [REMOVE] ({ commit }, id) {
+      commit(REMOVE, id)
+    }
+  }
+
+  const mutations = {
+    [ADD] (state: KetchupState, data: KetchupNotification) {
+      state.notifications.push(data)
+    },
+
+    [REMOVE] (state: KetchupState, notification) {
+      state.notifications = state.notifications.filter(n => n.id !== notification.id)
+    }
+  }
+
+  return {
+    state,
+    getters,
+    actions,
+    mutations
+  }
+}
