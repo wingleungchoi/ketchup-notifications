8a6bc82d3f9672fe5c77bfa1047a133b95af5914
wingleungCHOI <wingleungchoi@gmail.com>
2017-08-23 16:57:40 +0800

    publish/1 remove test case

--- src/test.js
+++
@@ -1,5 +0,0 @@
-var ketchup = require('../dist/bundle.js')
-console.log('ketchup', ketchup)
-
-console.log('KetchupNotifications', ketchup.KetchupNotifications)
-console.log('createComponent', ketchup.createComponent)
